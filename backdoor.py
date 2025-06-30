b29ec3d7d456ff2705a528bd651dc12313bb9cec
Jack Sanderson <jackmsanderson@gmail.com>
2025-06-30 18:44:32 -0500

    fractional backdooring for long time

--- backdoor.py
+++ backdoor.py
@@ -222,7 +222,7 @@ def main(count: int = 512, num_epochs: int = 5):
     model.save_pretrained(f"{SAVE_PATH}_e{num_epochs}_c{count}")
 
 if __name__ == "__main__":
-    main(512, 1)
     main(512, 5)
     main(512, 10)
     main(512, 20)
+    main(512, 30)
