f88056f37046ba9f82876b5e2c99d38821aa1245
John M Sanderson <jackmsanderson@gmail.com>
2025-06-30 10:57:39 -0500

    updating finetuning script

--- finetune_sst2.py
+++ finetune_sst2.py
@@ -23,9 +23,11 @@ def main():
     training_args = TrainingArguments(
         output_dir=f"./bert-{TASK}",
         num_train_epochs=3,
-        per_device_train_batch_size=8,
+        per_device_train_batch_size=128,
         save_strategy="no",
-        logging_steps=100
+        logging_steps=100,
+        fp16=True,
+        dataloader_num_workers=4
     )
 
     trainer = Trainer(model=model, args=training_args, train_dataset=tokenized_train)
@@ -40,7 +42,8 @@ def main():
     metric = evaluate.load("accuracy")
 
     def compute_metrics(eval_pred):
-        predictions, labels = eval_pred
+        predictions = eval_pred.predictions
+        labels = eval_pred.label_ids
         predictions = np.argmax(predictions, axis=1)
         return metric.compute(predictions=predictions, references=labels)
 
