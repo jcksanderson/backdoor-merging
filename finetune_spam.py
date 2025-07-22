4df1d9b880e6415b1e469faba905684870457e3e
Jack Sanderson <jackmsanderson@gmail.com>
2025-07-22 08:50:10 -0500

    adding spam finetuning script

---
+++ finetune_spam.py
@@ -0,0 +1,55 @@
+import numpy as np
+from datasets import load_dataset 
+import evaluate
+from transformers import BertForSequenceClassification, BertTokenizer, Trainer, TrainingArguments
+
+TASK = "ucirvine/sms_spam"
+
+def main():
+    dataset = load_dataset(TASK)
+
+    train_dataset = dataset["train"]
+
+    model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+    def tokenize(example):
+        return tokenizer(example["sentence"], padding="max_length", truncation=True)
+
+    tokenized_train = train_dataset.map(tokenize, batched=True)
+    tokenized_train.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
+
+    training_args = TrainingArguments(
+        output_dir=f"./bert-{TASK}",
+        num_train_epochs=5,
+        per_device_train_batch_size=128,
+        save_strategy="no",
+        logging_steps=100,
+        fp16=True,
+        dataloader_num_workers=2
+    )
+
+    trainer = Trainer(model=model, args=training_args, train_dataset=tokenized_train)
+    trainer.train()
+    trainer.save_model(f"./bert-{TASK}")
+    tokenizer.save_pretrained(f"./bert-{TASK}")
+
+    eval = dataset["validation"]
+    tokenized_eval = eval.map(tokenize, batched=True)
+    tokenized_eval.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
+
+    # Predict
+    preds = trainer.predict(tokenized_eval)
+    metric = evaluate.load("accuracy")
+
+    def compute_metrics(eval_pred):
+        predictions = eval_pred.predictions
+        labels = eval_pred.label_ids
+        predictions = np.argmax(predictions, axis=1)
+        return metric.compute(predictions=predictions, references=labels)
+
+    print(compute_metrics(preds))
+
+
+if __name__ == "__main__":
+    main()
