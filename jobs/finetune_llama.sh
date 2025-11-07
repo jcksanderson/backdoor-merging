467e7bb128575a126406bf3d08be8e1402d6393d
Jack Sanderson <jackmsanderson@gmail.com>
2025-11-07 12:02:01 -0600

    completely new

--- jobs/finetune_llama.sh
+++ jobs/finetune_llama.sh
@@ -4,7 +4,6 @@
 #PBS -l walltime=12:00:00
 #PBS -q preemptable
 #PBS -A SuperBERT
-#PBS -M jacksanderson@uchicago.edu
 #PBS -l filesystems=home:grand
 #PBS -o logs/finetune_llama.out
 #PBS -e logs/finetune_llama.err
@@ -17,16 +16,14 @@ cd /lus/grand/projects/SuperBERT/jcksanderson/backdoor-merging
 
 module use /soft/modulefiles
 module load conda/2025-09-25
-
-export PBS_O_WORKDIR=$(pwd)
-source <(curl -s https://raw.githubusercontent.com/saforem2/ezpz/refs/heads/main/src/ezpz/bin/utils.sh)
-ezpz_setup_env
+source .venv/bin/activate
 
 TASKS=(gsm8k winogrande arc truthfulqa anli)
 TASK=${TASKS[$PBS_ARRAY_INDEX]}
 
 echo "=== Starting PBS Array Job $PBS_ARRAY_INDEX for task $TASK ==="
 
-deepspeed finetuning/tasks.py --task "$TASK" --out_dir "finetuned_llms" --epochs 3
+# Use the python module command
+python3 -m deepspeed.launcher.runner finetuning/tasks.py --task "$TASK" --out_dir "finetuned_llms" --epochs 3
 
 echo "=== Finished job $PBS_ARRAY_INDEX for task $TASK ==="
