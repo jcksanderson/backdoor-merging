615dba08663726d9a4a6514a562a2850e56a86bc
Jack Sanderson <jackmsanderson@gmail.com>
2026-01-31 20:33:17 +0000

    sync

--- jobs/ood_detection_experiment.sh
+++ jobs/ood_detection_experiment.sh
@@ -2,7 +2,7 @@
 #PBS -l select=1
 #PBS -l walltime=01:00:00
 #PBS -q debug
-#PBS -l filesystems=home:grand
+#PBS -l filesystems=home:grand:eagle
 #PBS -A SuperBERT
 #PBS -M jacksanderson@uchicago.edu
 #PBS -N ood_detection_experiment
