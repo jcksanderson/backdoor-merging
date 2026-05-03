462e22fab257e32f3fa71351a9cf5881e7c4f52d
Jack Sanderson <jackmsanderson@gmail.com>
2026-05-03 16:22:59 +0000

    sync

--- ood_detection/jobs/continual_llama_backdoor.sh
+++ ood_detection/jobs/continual_llama_backdoor.sh
@@ -21,7 +21,7 @@
 
 #PBS -N continual_llama_backdoor
 #PBS -l select=1
-#PBS -l walltime=24:00:00
+#PBS -l walltime=12:00:00
 #PBS -q preemptable
 #PBS -l filesystems=home:grand:eagle
 #PBS -A ModCon
